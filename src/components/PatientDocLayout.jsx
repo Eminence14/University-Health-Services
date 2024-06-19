7682154f03e2c207f5ad2ff522ecbb1e569d1f77
V <victoribidunmoye@gmail.com>
2024-06-19 12:03:36 +0100

    diagnosis bar added functionality included

--- src/components/PatientDocLayout.jsx
+++ src/components/PatientDocLayout.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Card from "./Card";
 import search from "../assets/images/search.svg";
 import save from "../assets/images/save.svg";
+import close from "../assets/images/close.svg";
+import add from "../assets/images/add.svg"
+import diagnosisData from "../assets/diagnosis.json";
 
 const PatientDocLayout = () => {
+	const data = useMemo(() => diagnosisData, []);
+	const [selectedDiag, setSelectedDiag] = useState([]);
+	function handleSelectionChange(selectedValue) {
+		setSelectedDiag(selectedDiag.includes(selectedValue) ? [...selectedDiag] : [...selectedDiag, selectedValue]);
+	}
+	function removeDiag(e) {
+		const rmParam = e.target.innerText;
+		setSelectedDiag(selectedDiag.filter((param) => param !== rmParam));
+	}
 	return (
 		<div className="patient-doc-layout">
 			<section className="header">
@@ -23,14 +35,25 @@ const PatientDocLayout = () => {
 				</section>
 				<section className="mini-capsule diagnosis">
 					<h4>Diagnosis</h4>
-					<div className="searchBox">
-						<img src={search} alt="" />
-						<input type="text" placeholder="Search" />
+					<div className=" searchBox">
+						{selectedDiag.map((diagnosis) => (
+							<div className="diag-btn" key={diagnosis} onClick={(e) => removeDiag(e)}>
+								{diagnosis}
+								<img src={close} alt="close" className="rm-btn" />
+							</div>
+						))}
 					</div>
-					<select name="diagnosis" id="diagnosis">
-						<option value="1">ffyufuf</option>
-						<option value="2">ufujvjj</option>
-						<option value="3">cxhctu</option>
+					<select
+						multiple={true}
+						name="diagnosis"
+						id="diagnosis"
+						onChange={(e) => handleSelectionChange(e.target.value)}
+					>
+						{data.map((diagnosis) => (
+							<option key={diagnosis.id} value={diagnosis.diagnosis}>
+								{diagnosis.diagnosis}
+							</option>
+						))}
 					</select>
 				</section>
 				<section className="mini-capsule prescription">
